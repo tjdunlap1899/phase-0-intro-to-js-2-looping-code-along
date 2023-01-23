895dc1b71d5bf6d80ae7cec1e326887b1e621341
Trevor Dunlap <tjdunlap1899@gmail.com>
2023-01-23 16:38:51 -0500

    commit tree e8c8ffb4575bb1a1b807e68f0d75c6d0b8553235

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solutions in this file
+// for (let age = 30; age < 40; age++) {
+    // console.log(`I'm ${age} years old. Happy birthday to me!`);
+    //debugger;
+// }
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+    // for (let i = 0; i < gifts.length; i++) {
+        // console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    // }
+
+    // return gifts;
+// }
+
+// wrapGifts(gifts);
+
+const namesList = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(namesList) {
+    let thankYouCards = [];
+    for (let i = 0; i < namesList.length; i++) {
+        thankYouCards.push(`Thank you, ${namesList[i]}, for the wonderful surprise gift!`);
+    }
+    return thankYouCards
+}
+
+function countDown(startingNumber) {;
+while (startingNumber > 0) {
+    console.log(startingNumber);
+    startingNumber -= 1;
+}
+console.log(startingNumber);
+}
